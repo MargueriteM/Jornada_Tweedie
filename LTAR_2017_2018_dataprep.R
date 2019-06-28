56d9312155a954060edafa343db9d600e40c9b9d
Marguerite Mauritz <mauritzmarguerite@gmail.com>
2019-06-28 10:24:49 -0600

    code started

---
+++ LTAR_2017_2018_dataprep.R
@@ -0,0 +1,10 @@
+###################################
+#    Met data compilation for     #
+#  LTAR Synthesis, Dawn Browining #
+#       2017 and 2018             #
+# written by: M. Mauritz          #
+# 28 June 2019                    #
+###################################
+
+# data loaded from Eddy Pro Biomet data prep in: Jornada_EnvironmentalData_Combine.R
+# metadata and data templates at: 
