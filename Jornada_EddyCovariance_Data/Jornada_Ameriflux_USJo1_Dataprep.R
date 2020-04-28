3b8e80b0d3eb33aa197e48814f1c4489b73ca86e
Marguerite Mauritz <mauritzmarguerite@gmail.com>
2020-04-27 18:06:07 -0600

    Update to fix timestamps in 30min fluxes and biomet

--- Jornada_EddyCovariance_Data/Jornada_Ameriflux_USJo1_Dataprep.R
+++ Jornada_EddyCovariance_Data/Jornada_Ameriflux_USJo1_Dataprep.R
@@ -5,6 +5,8 @@
 #             August 2019                  #
 ############################################
 
+# 20200427: update with timestamp corrected biomet and flux data
+
 library(data.table)
 library(lubridate)
 library(ggplot2)
@@ -14,41 +16,65 @@ library(bit64)
 # filtered in: Jornada_EddyPro_Output_Fluxnext_2010_2019.R
 setwd("~/Desktop/TweedieLab/Projects/Jornada/EddyCovariance/JER_Out_EddyPro_filtered")
 
-# import data that was filtered by 3SD filter
-load("JER_flux_2010_2019_EddyPro_Output_filtered_SD_20200212.Rdata")
+# import data that was filtered by 3SD filter with timestamp corrected
+load("JER_flux_2010_2019_EddyPro_Output_filtered_SD_TIMEcorr_20200427.Rdata")
+
+# rename columns
+# TIMESTAMP_START = TIMESTAMP_START_correct
+# TIMESTAMP_END = TIMESTAMP_END_correct
+setnames(flux_filter_sd,c("TIMESTAMP_START_correct","TIMESTAMP_END_correct"),
+         c("TIMESTAMP_START","TIMESTAMP_END"))
 
 # convert date to POSIXct and get a year, day, hour column
-# if this step doesn't work, make sure bit64 library is loaded otherwise the timestamps importa in a non-sensical format
-flux_filter_sd[,':=' (date_time = parse_date_time(TIMESTAMP_START,"YmdHM",tz="UTC"))]
+# if this step doesn't work, make sure bit64 library is loaded otherwise the timestamps import in a non-sensical format
+flux_filter_sd[,date_time := NULL]
+flux_filter_sd[,':=' (date_time = parse_date_time(TIMESTAMP_END,"YmdHM",tz="UTC"))]
 
 # there's duplicated data in 2012 DOY 138
 flux <- (flux_filter_sd[!(duplicated(flux_filter_sd, by=c("date_time")))])
 
 
 # import biomet2 which contains all sensors as individual datastreams
-setwd("~/Desktop/TweedieLab/Projects/Jornada/EddyCovariance/MetDataFiles_EP")
+setwd("~/Desktop/TweedieLab/Projects/Jornada/EddyCovariance/MetDataFiles_EP/Biomet2_20200415")
 
-biomet2 <- fread(file="Biomet_USJo1_wide_2010_2019_20200214.csv", sep=",", dec=".", na.strings=c("NA","NaN"),
-                 header=TRUE)
+biometfiles <- list.files(path="~/Desktop/TweedieLab/Projects/Jornada/EddyCovariance/MetDataFiles_EP/Biomet2_20200415",
+                          full.names=TRUE, pattern="_wide_") 
 
-biomet2[,':=' (date_time = parse_date_time(date_time,"Ymd HMS",tz="UTC"))][,TIMESTAMP]
+# read files and bind them into one file. fill=TRUE because of the missing columns in 2011
+biomet_all <- do.call("rbind", lapply(biometfiles, header = TRUE, fread, sep=",",fill=FALSE))
+
+biomet_all <- (biomet_all[!(duplicated(biomet_all, by=c("date_time")))])
+
+                                   
+biomet_all[,':=' (date_time = parse_date_time(date_time,"Ymd HMS",tz="UTC"))]
 
 # merge the flux data and biomet2 data
 # first remove the biomet columns from the flux data
 exclude_cols <- c("TA_1_1_1", "RH_1_1_1", "PA_1_1_1", "WD_1_1_1", "MWS_1_1_1", "PPFD_IN_1_1_1", "PPFD_OUT_1_1_1", 
                   "P_RAIN_1_1_1", "SWC_1_1_1", "TS_1_1_1", "G_1_1_1", "G_1_2_1", "G_2_1_1", "G_2_2_1",
-                  "LW_IN_1_1_1", "LW_OUT_1_1_1", "SW_OUT_1_1_1", "SW_IN_1_1_1", "NETRAD_1_1_1", "date",
-                  "month","year") 
+                  "LW_IN_1_1_1", "LW_OUT_1_1_1", "SW_OUT_1_1_1", "SW_IN_1_1_1", "NETRAD_1_1_1", "date_orig",
+                  "month_orig","year_orig") 
 
 flux <- flux[,!c(exclude_cols),with=FALSE]
 
-flux.biomet <- merge(flux,biomet2, by="date_time", all.x=TRUE)
+flux.biomet <- merge(flux,biomet_all, by="date_time", all.x=TRUE)
 
 # save to upload to ameriflux: 
 # <SITE_ID>_<RESOLUTION>_<TS-START>_<TS-END>_<OPTIONAL>.csv
 setwd("~/Desktop/TweedieLab/Projects/Jornada/EddyCovariance/Ameriflux")
 
-write.table(flux.biomet, paste("USJo1_HH",min(flux.biomet$TIMESTAMP_START),max(flux.biomet$TIMESTAMP_START),
-                               "20200214submit.csv",sep="_"), sep=',', dec='.', row.names=FALSE)
+write.table(flux.biomet[,!c("date_time"),with=FALSE], paste("USJo1_HH",min(flux.biomet$TIMESTAMP_END),max(flux.biomet$TIMESTAMP_END),
+                               "20200427submit.csv",sep="_"), sep=',', dec='.', row.names=FALSE)
+
+# save by years
+for (i in 2010:2019){
+  # subset each year
+  dat.save <- flux.biomet[year(date_time)==i,]
+  
+  write.table (dat.save[,!c("date_time"),with=FALSE],
+             file= paste("USJo1_HH",min(dat.save$TIMESTAMP_END),max(dat.save$TIMESTAMP_END),
+                         "20200427submit.csv",sep="_"),
+             sep =',', dec='.', row.names=FALSE, na="-9999", quote=FALSE)
+}
 
 
