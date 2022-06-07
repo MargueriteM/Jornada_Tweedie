dfc42fd67fd80866a4793b390e37ca479ac1266d
Marguerite Mauritz <mauritzmarguerite@gmail.com>
2022-06-07 13:20:27 -0600

    complete QA/QC for 2021

--- Jornada Climate Data/Data_QAQC_update_save_Climate_dates.R
+++ Jornada Climate Data/Data_QAQC_update_save_Climate_dates.R
@@ -105,13 +105,11 @@ ggplot(climate_30min, aes(date_time, e))+geom_line()
 ggplot(climate_30min, aes(date_time, atm_press))+geom_line()
 ggplot(climate_30min, aes(date_time, wnd_spd))+geom_line()
 ggplot(climate_30min, aes(date_time, wnd_dir))+geom_line()
-climate_wind <- copy(climate_30min[!is.na(wnd_dir)|!is.na(wnd_spd),])
+climate_wind <- copy(climate_30min[!is.na(wnd_dir) | !is.na(wnd_spd),])
 plot.windrose(climate_wind,climate_wind$wnd_spd, climate_wind$wnd_dir)
 
-fig.precip <- ggplot(climate_30min, aes(date_time, precip_tot))+geom_line()
-fig.precip+scale_x_datetime(date_breaks="1 month", date_labels="%b")+labs(title="Bajada US-Jo1 Rainfall, 2021", y="Total 30 min Rainfall (mm)")
+fig.precip <- ggplot(climate_30min, aes(date_time, precip_tot))+geom_line()+scale_x_datetime(date_breaks="1 month", date_labels="%b")+labs(title="Bajada US-Jo1 Rainfall, 2021", y="Total 30 min Rainfall (mm)")
 fig.lws <- ggplot(climate_30min, aes(date_time, lws_5m))+geom_line()
-fig.lws 
 
 # Precip: check patterns with LWS to see if events are misssing.
 # LWS and Rain should have similar appearance of spikes
@@ -163,7 +161,8 @@ lws.max <- max(met30_long[variable %in% c("lws_5m") & !is.na(value),]$value)
 # RESCALE from 0 to 100
 met30_long[variable %in% c("lws_5m"), value := ((value-250)/(375-250))*100]
 
-ggplot(met30_long[variable=="lws_5m"], aes(date_time, value))+geom_point()+labs(title="lws_5m")
+grid.arrange(fig.precip,
+             ggplot(met30_long[variable=="lws_5m"], aes(date_time, value))+geom_point()+labs(title="rescale lws_5m"))
 
 
 # albedo
