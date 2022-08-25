33c2d338ab6156df83bce73c387fd3620135f2e2
Marguerite Mauritz <mauritzmarguerite@gmail.com>
2022-08-25 14:10:24 -0600

    comment out windrose.

--- Jornada Climate Data/Data_QAQC_update_save_Climate_dates.R
+++ Jornada Climate Data/Data_QAQC_update_save_Climate_dates.R
@@ -23,7 +23,6 @@ library(plotly)
 
 
 # get windrose function, source directly from Github
-#source("~/Desktop/R/R_programs/Functions/plot.windrose.R")
 source(paste0("https://raw.githubusercontent.com/MargueriteM/R_functions/master/plot.windrose.R"))
 
 ## windrose function on Marguerite's local computer, in case internet doesn't work
